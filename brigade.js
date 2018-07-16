0e594c4e727634a0763be00a2805272efe6da7a5
Lachlan Evenson <lachlan.evenson@microsoft.com>
2018-07-16 14:16:45 -0700

    update helm command

--- brigade.js
+++ brigade.js
@@ -13,6 +13,7 @@ events.on("push", (brigadeEvent, project) => {
     var gitSHA = brigadeEvent.revision.commit.substr(0,7)
     var imageTag = "master-" + String(gitSHA)
     var acrImage = image + ":" + imageTag
+    var helmReleaseNamespace = default
 
     console.log("started")
 
@@ -29,7 +30,7 @@ events.on("push", (brigadeEvent, project) => {
     helm.storage.enabled = false
     helm.image = "lachlanevenson/k8s-helm:v2.9.1"
     helm.tasks = [
-        `helm upgrade --install --reuse-values kubecon ./src/app/web/charts/kubecon-rating-web --set image=${acrServer}/${image} --set imageTag=${imageTag}`
+        `helm upgrade --install --reuse-values oscon ./src/app/web/charts/kubecon-rating-web --set image=${acrServer}/${image} --set imageTag=${imageTag} --namespace ${helmReleaseNamespace}`
     ]
 
     var pipeline = new Group()
